1bcdcb97aac6c7c7983044617adf7d360e5e1ab6
Divyanshu30P <divyanshuprajapati3008@gmail.com>
2025-12-02 16:57:41 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,20 @@
-import os
 from telegram.ext import Updater, MessageHandler, Filters
-from googletrans import Translator
-
-translator = Translator()
+from deep_translator import GoogleTranslator
+import os
 
 def translate_message(update, context):
     text = update.message.text
-
     try:
-        result = translator.translate(text, dest='hi')
-        update.message.reply_text(result.text)
+        result = GoogleTranslator(source='auto', target='hi').translate(text)
+        update.message.reply_text(result)
     except:
-        update.message.reply_text("Translation failed, try again.")
+        update.message.reply_text("Translation failed")
 
 def main():
     token = os.environ["BOT_TOKEN"]
-
     updater = Updater(token, use_context=True)
     dp = updater.dispatcher
-
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, translate_message))
-
     updater.start_polling()
     updater.idle()
 
